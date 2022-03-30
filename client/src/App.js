4f2ff9babf96a48f1b0a5f3a6cb260e2c7684ce9
Victor <86551854+devDalys@users.noreply.github.com>
2022-03-30 18:03:06 +0300

    fetch is ready, but response undef, need fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {useQuery} from "@apollo/client";
+import {GET_ALL_USERS} from "./query/user";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {loading, error, data} = useQuery(GET_ALL_USERS)
+    console.log(data);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+
+    },[data])
+    return (
+        <div className="App">
+            <form>
+                <input type="text"/>
+                <input type="number"/>
+                <div className="btns">
+                    <button>Создать</button>
+                    <button>Получить</button>
+                </div>
+
+            </form>
+            <div>{users.map(user =>
+                <div className="user">
+                    {user.id} {user.username} {user.age}
+                </div>
+            )}</div>
+        </div>
+    );
 }
 
 export default App;
