ee4434bedd1ff0e429f7ebfd7eb6c29c5b96508a
Victor <86551854+devDalys@users.noreply.github.com>
2022-03-31 15:25:23 +0300

    createrUser added

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,52 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {useQuery,gql} from "@apollo/client";
+import {useQuery, gql, useMutation} from "@apollo/client";
+import CircularProgress from '@mui/material/CircularProgress';
 import {GET_ALL_USERS, GET_ONE_USER} from "./query/user";
+import {CREATE_USER} from "./mutation/user";
 
 function App() {
-    const {data, loading, error} = useQuery(gql`
-    query {
-      getAllUsers {
-        id
-        username
-        age
-      }
-}
-`)
+    const {data, loading, error} = useQuery(GET_ALL_USERS)
     const [users, setUsers] = useState([]);
-    console.log(data,loading,error);
-    // useEffect(() => {
-    // },[data])
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState(0);
+    const [newUser] = useMutation(CREATE_USER);
+    const addUser = (e) => {
+        e.preventDefault();
+        newUser({
+            variables: {
+                input: {username, age}
+            }
+        }).then(({data}) => {
+            console.log(data);
+            setUsername('');
+            setAge(0);
+        })
+    }
+    useEffect(() => {
+        if (!loading) {
+            setUsers(data.getAllUsers)
+        }
+    }, [data])
+    if (loading) return <CircularProgress/>
     return (
         <div className="App">
             <form>
-                <input type="text"/>
-                <input type="number"/>
+                <input placeholder={('Имя')} value={username} onChange={(e) => setUsername(e.target.value)}
+                       type="text"/>
+                <input placeholder={('Возраст')} value={age} onChange={(e) => setAge(e.target.valueAsNumber)} type="number"/>
                 <div className="btns">
-                    <button>Создать</button>
+                    <button onClick={(event) => {
+                        addUser(event);
+                    }}>Создать
+                    </button>
                     <button>Получить</button>
                 </div>
 
             </form>
-            <div>{users.map(user =>
-                <div className="user">
+            <div>{users.map( user =>
+
+                <div key={user.id} className="user">
                     {user.id} {user.username} {user.age}
                 </div>
             )}</div>
